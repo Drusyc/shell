e8f0cf030529654ef129606f7209649f3245b90b
Thomas <guntho.neo@gmail.com>
2015-02-13 16:55:14 +0100

    on se tient au jus pendant la semaine de vacances :)

--- src/ensishell.c
+++ src/ensishell.c
@@ -7,6 +7,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> // fork()..
+#include <sys/types.h> //wait()..
+#include <sys/wait.h> //wait()..
 
 #include "variante.h"
 #include "readcmd.h"
@@ -14,6 +17,18 @@
 #ifndef VARIANTE
 #error "Variante non défini !!"
 #endif
+
+int execCmd (struct cmdline * l, int i) {
+        int fpid = -1;
+
+        if ((fpid = fork()) == 0) {
+                execvp(*(l->seq[i]), *l->seq);
+        } else if (l->bg) {
+                wait(NULL);
+        }
+        return 0;
+}//execCmd()
+
 
 int main() {
         printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
@@ -44,11 +59,15 @@ int main() {
 		/* Display each command of the pipe */
 		for (i=0; l->seq[i]!=0; i++) {
 			char **cmd = l->seq[i];
+
+                        if (execCmd(l, i) != 0)
+                                perror ("FAUX");
+
 			printf("seq[%d]: ", i);
                         for (j=0; cmd[j]!=0; j++) {
                                 printf("'%s' ", cmd[j]);
-                        }
+                        }//for()
 			printf("\n");
-		}
-	}
-}
+		}//for()
+	}//while()
+}//main
